5925407a0b4b7186d87132ed0801d255452782a9
Cain Atkinson <yellowsink@riseup.net>
2022-03-10 21:27:09 +0000

    first inital draft

--- src/AboutMe.svelte
+++ src/AboutMe.svelte
@@ -1,4 +1,11 @@
 <!-- separation of concerns baby -->
+
 <p>
-  &lt;Put things here&gt;
+  I am a software developer in my free time, and have ~4 years of programming.
+  For the past 6 months I have primarily been building plugins for Discord,
+  which is a good source of experience for JS and React.
+</p>
+<p>
+  I am comfortable on the command line, and like to customise my environment
+  to be as comfortable for me.
 </p>
